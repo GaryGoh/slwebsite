409918eedf8542d13a4ed328e752e70b72da2a0f
GaryG <gaomingqian90929@gmail.com>
2013-11-07 11:14:40 +0000

    new slwebsite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Slwebsite::Application.config.secret_key_base = '1fe4d92d89e03eec1f28adbbc94dc324ad6257e26e4c029a801a3e66cee4e2a01d32e8175f49d2b5fdd58224030f24871415ab633925d2b8bf35d8217b1cb213'
