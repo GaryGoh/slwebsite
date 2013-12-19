2b66656b982404fa5b2c0594d1b1f16860880ddb
GaryG <gaomingqian90929@gmail.com>
2013-12-19 01:08:54 +0000

    refine css

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,5 +1,5 @@
 class StaticPageController < ApplicationController
-  layout "index"
+  layout "index", only: [:index]
 
   def home
     @issues_school = Issue.where('category_id = 1').last(5).reverse
