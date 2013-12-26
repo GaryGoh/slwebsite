e29f66772fbc8b44857b358a1aed9f9dd48e2a52
GaryG <gaomingqian90929@gmail.com>
2013-12-26 22:12:18 +0000

    refine society back-end

--- app/admin/society.rb
+++ app/admin/society.rb
@@ -36,7 +36,7 @@ ActiveAdmin.register Society do
 
 
   show :title => :society_name do |c|
-    @society = Society.find_by_id(params[:id])
+    #@society = Society.find_by_id(params[:id])
     columns do
       column do
 
@@ -68,95 +68,94 @@ ActiveAdmin.register Society do
           end
         end
 
-      #  div :class => "panel" do
-      #    h3 "所属社团"
-      #    if c.user_societies and c.user_societies.count > 0
-      #      div :class => "panel_contents" do
-      #        div :class => "attributes_table" do
-      #          table do
-      #            th do
-      #              "所属中心"
-      #            end
-      #            th do
-      #              "社团名字"
-      #            end
-      #            th do
-      #              "加入时间"
-      #            end
-      #
-      #            tbody do
-      #              c.user_societies.each do |s|
-      #                tr do
-      #                  td do
-      #                    link_to s.society.department, admin_department_path(s.society.department.id)
-      #                  end
-      #                  td do
-      #                    link_to s.society, admin_society_path(s.society.id)
-      #                  end
-      #                  td do
-      #                    s.created_at.strftime("%F")
-      #                  end
-      #                end
-      #              end
-      #            end
-      #          end
-      #        end
-      #      end
-      #    else
-      #      h3 "尚未加入社团"
-      #    end
-      #  end # end of {div :class => "panel" do}
-      #end
-      #
-      #column do
-      #
-      #  h3 "发布新闻"
-      #  table class: "index_table index" do
-      #    tr do
-      #      th "新闻标题"
-      #      th "发布时间"
-      #      th "新闻类型"
-      #      th "评论数"
-      #      th "阅读次数"
-      #    end
-      #    @user.issues.each do |e|
-      #      tr class: odd ? "odd" : "even" do
-      #        td link_to e.title, admin_issue_path(e.id)
-      #        td e.created_at.strftime("%b %e, %l:%M %p")
-      #        td e.category
-      #        td e.title
-      #        td e.title
-      #
-      #      end
-      #    end
-      #  end
-      #end # end of #2 column
 
-      end
 
+      div :class => "panel" do
+        h3 "新加入会员"
+        if c.user_societies and c.user_societies.count > 0
+          div :class => "panel_contents" do
+            div :class => "attributes_table" do
+              table do
+                th do
+                  "会员学号"
+                end
+                th do
+                  "会员名字"
+                end
+                th do
+                  "加入时间"
+                end
+
+                tbody do
+                  c.user_societies.each do |s|
+                    tr do
+                      td do
+                        link_to s.user.stuid, admin_user_path(s.user.id)
+                      end
+                      td do
+                        link_to s.user.name, admin_society_path(s.user.id)
+                      end
+                      td do
+                        s.created_at.strftime("%F")
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end # end of {div :class => "panel" do}
     end
+
+    column do
+
+      h3 "发布新闻"
+      table class: "index_table index" do
+        tr do
+          th "新闻标题"
+          th "新闻类型"
+          th "作者"
+          th "发布时间"
+          th "阅读次数"
+        end
+        c.issues.each do |e|
+          tr do
+            td link_to truncate(e.title, :length => 12), admin_issue_path(e.id)
+            td e.category
+            td link_to e.author, admin_user_path(e.user.id)
+            td e.created_at.strftime("%b %e, %l:%M %p")
+            td e.title
+
+          end
+        end
+      end
+    end # end of #2 column
+
   end
+end
 
 
-  filter :society_name, :label => "社团名字"
-  filter :is_outstanding, :label => "是否为优秀社团"
-  filter :num_member, :label => "社团会员数"
-  filter :num_registered_member, :label => "网站会员数"
-  filter :issues, :label => "社团新闻"
-  filter :created_at, :label => "创建于"
+
+filter :society_name, :label => "社团名字"
+filter :is_outstanding, :label => "是否为优秀社团"
+filter :num_member, :label => "社团会员数"
+filter :num_registered_member, :label => "网站会员数"
+filter :issues, :label => "社团新闻"
+filter :created_at, :label => "创建于"
 
 
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+# See permitted parameters documentation:
+# https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#  permitted = [:permitted, :attributes]
+#  permitted << :other if resource.something?
+#  permitted
+# end
 
 end
