46df42178c8070dae4f119f38782b79f65f83a34
GaryG <gaomingqian90929@gmail.com>
2013-12-29 05:27:36 +0000

    fix lightbox

---
+++ app/assets/javascripts/blueimp.js
@@ -0,0 +1,11 @@
+//=require blueimp/blueimp-gallery
+//=require blueimp/blueimp-gallery.min
+//=require blueimp/blueimp-gallery-fullscreen
+//=require blueimp/blueimp-gallery-indicator
+//=require blueimp/blueimp-gallery-video
+//=require blueimp/blueimp-gallery-vimeo
+//=require blueimp/blueimp-gallery-youtube
+//=require blueimp/blueimp-helper
+//=require blueimp/jquery.blueimp-gallery
+//=require blueimp/jquery.blueimp-gallery.min
+
