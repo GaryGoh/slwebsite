011f336ecbdf6c7a5ba6d8b8d93edd5269f8db3d
GaryG <gaomingqian90929@gmail.com>
2014-05-03 01:32:33 +0100

    merge

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require googleajax.jquery.js
 //= require turbolinks
 //= require_tree .
 
@@ -30,6 +31,35 @@
 //= require jquery.goup.min
 //= require popup
 
+
+
+
+
+
+
+
+
+
+
+// To adjust the params of right side panel
+$(document).ready(function () {
+    $(".cbp-af-header").css("overflow", "hidden");
+
+    $('#sidebar').affix({
+        offset: {
+            top: 230,
+            bottom: 100
+        }
+    });
+
+    $('#midCol').affix({
+        offset: {
+            top: 800,
+            bottom: 100
+        }
+    });
+
+});
 
 
 
