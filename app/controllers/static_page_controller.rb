ebcafcfbf37c89fab0fce83ed7a8cbeebaf2e983
GaryG <gaomingqian90929@gmail.com>
2014-09-23 09:37:35 +0100

    fix will_pagniate & activeadmin conflicts

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -50,7 +50,7 @@ class StaticPageController < ApplicationController
     @xs = Society.where('department_id = 4').order("RANDOM()")
 
     # Index User's track
-    unless current_user_stu.nil? && session[:user_id].nil?
+    unless session[:user_id] = ""
       @last_current_user_id = Impression.where(:user_id => current_user_stu.id).reverse
     end
 
@@ -78,10 +78,17 @@ class StaticPageController < ApplicationController
       @w2.push(Impression.where("created_at BETWEEN ? AND ?",
                                 Date.today.to_time - (14 - i).day, Date.today.to_time - (14 - i - 1).day).reverse)
     end
+
+    @week1 = Impression.where("created_at BETWEEN ? AND ?",
+                              Date.today.to_time - 7.day, Date.today.to_time).reverse
+    @w1 = Array.new()
+    for i in 0..7
+      @w1.push(Impression.where("created_at BETWEEN ? AND ?",
+                                Date.today.to_time - (7 - i).day, Date.today.to_time - (7 - i - 1).day).reverse)
+    end
     #@lastest_news = Issue.find(:id => 16)
 
   end
-
 
 
   def about
