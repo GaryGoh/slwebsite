11b9fa83889dabc647769246f0cfe7ce3a24af6f
GaryG <gaomingqian90929@gmail.com>
2014-09-24 12:00:03 +0100

    fix bugs

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -50,7 +50,7 @@ class StaticPageController < ApplicationController
     @xs = Society.where('department_id = 4').order("RANDOM()")
 
     # Index User's track
-    unless current_user_stu.nil? && session[:user_id].nil?
+    unless current_user_stu.nil?
       @last_current_user_id = Impression.where(:user_id => current_user_stu.id).reverse
     end
 
