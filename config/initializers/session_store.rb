b4683f3be4038ff6457b9be737cc23dc8adb5d7e
GaryG <gaomingqian90929@gmail.com>
2014-09-24 10:52:01 +0100

    session bugs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Slwebsite::Application.config.session_store :cookie_store, key: '_slwebsite_session'
+Slwebsite::Application.config.session_store :cookie_store, {key: '_slwebsite_session',
+                                                            :expire_after => 30.minutes}
