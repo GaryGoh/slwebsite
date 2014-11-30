4db7605d24cf49cbfbabf2f45537161a2bfb5583
GaryG <gaomingqian90929@gmail.com>
2014-11-30 12:15:26 +0000

    add comment at sys back end

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901173255) do
+ActiveRecord::Schema.define(version: 20141130120527) do
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "admin_permissions", force: true do |t|
     t.string   "permission_name"
@@ -45,14 +60,23 @@ ActiveRecord::Schema.define(version: 20140901173255) do
     t.string   "admin_pic_content_type"
     t.integer  "admin_pic_file_size"
     t.datetime "admin_pic_updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
   end
 
   add_index "admin_users", ["admin_permission_id"], name: "index_admin_users_on_admin_permission_id"
+  add_index "admin_users", ["confirmation_token"], name: "index_admin_users_on_confirmation_token", unique: true
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["gender_id"], name: "index_admin_users_on_gender_id"
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   add_index "admin_users", ["sl_department_id"], name: "index_admin_users_on_sl_department_id"
   add_index "admin_users", ["society_id"], name: "index_admin_users_on_society_id"
+  add_index "admin_users", ["unlock_token"], name: "index_admin_users_on_unlock_token", unique: true
 
   create_table "categories", force: true do |t|
     t.string   "category_name"
